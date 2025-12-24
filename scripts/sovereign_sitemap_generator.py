754aa284c1fa4a0b483fcd7d97f777b0812ffd23
yishen-global <alex.yang@yishenglobal.net>
2025-12-24 13:54:58 +0800

    Create sovereign_sitemap_generator.py

---
+++ scripts/sovereign_sitemap_generator.py
@@ -0,0 +1,16 @@
+import json
+from datetime import datetime
+
+BASE = "https://yishenglobal.net"
+
+PAGES = [
+    "genesis",
+    "clusters",
+    "industries",
+    "resources",
+    "technical-passport",
+    "tactical-briefing"
+]
+
+with open("sku-database.json", "r
+
